869552aa2d1e4425c0ad9ad674f30fe15b314641
Paige Gulley <nano3.14@gmail.com>
2023-06-15 17:28:16 -0400

    actually using the dataclasses on the children datums now, so we can catch ill use at runtime

--- indexer/story.py
+++ indexer/story.py
@@ -1,6 +1,6 @@
 import pickle
-from dataclasses import dataclass
-from typing import Any, Callable
+from dataclasses import dataclass, field, fields
+from typing import Any, Callable, Optional
 
 # A single story interface object, with typed data fields for each pipeline step,
 # context management on each of those step datum, and a serialization scheme.
@@ -9,34 +9,42 @@ from typing import Any, Callable
 
 # dataclass over typeddict so that we can instantiate it with a method
 # from the parent- keeps typing rules, added flexibility.
-@dataclass
+@dataclass(kw_only=True)
 class StoryDatum:
     dirty: bool = False
-
+    exit_cb: Callable = field(repr=False)
     # I'm not sure how get the cm behavior in
     # https://github.com/mediacloud/story-indexer/issues/6#issuecomment-1591589186
     # without a circular composition. It makes me a little gassy, but it works!
 
-    def __init__(self, exit_cb: Callable) -> None:
-        self.exit_cb = exit_cb
+    # def __init__(self, exit_cb: Callable) -> None:
+    #    self.exit_cb = exit_cb
 
-    # implimenting typing on return:self is really finicky, just doing Any for now
+    # implementing typing on return:self is really finicky, just doing Any for now
     def __enter__(self) -> Any:
         return self
 
     def __setitem__(self, key: str, value: Any) -> None:
+        if key not in [f.name for f in fields(self)]:
+            raise RuntimeError(f"Field {key} not defined for {self}")
         super().__setattr__(key, value)
         self.dirty = True
+
+    def __getitem__(self, attr: str) -> Any:
+        return getattr(self, attr)
 
     def __exit__(self, type: Any, value: Any, traceback: Any) -> None:
         self.exit_cb(self)
 
 
+# Weird clash between dataclass and mypy- only set to None so it can be instantiated empty,
+# but that then requires 'optional[str]' which isn't really pretty either.
+@dataclass(kw_only=True)
 class RSSEntry(StoryDatum):
-    link: str
-    title: str
-    domain: str
-    pub_date: str
+    link: Optional[str] = None
+    title: Optional[str] = None
+    domain: Optional[str] = None
+    pub_date: Optional[str] = None
 
 
 class BaseStory:
@@ -44,7 +52,7 @@ class BaseStory:
     rss_entry_data: RSSEntry
 
     def __init__(self) -> None:
-        self.rss_entry_data: RSSEntry = RSSEntry(self.rss_entry_cb)
+        self.rss_entry_data: RSSEntry = RSSEntry(exit_cb=self.rss_entry_cb)
 
     # Will need the following two stubs for each property.
     # Maybe a better way to manage this when something is
@@ -84,11 +92,15 @@ if __name__ == "__main__":
         rss_entry["title"] = sample_rss["title"]
         rss_entry["domain"] = sample_rss["domain"]
         rss_entry["pub_date"] = sample_rss["pub_date"]
-
+        print(rss_entry["link"])
         print(rss_entry)
+        try:
+            rss_entry["bad_attr"] = 0
+        except Exception:
+            print("Successfully prevented out of band attr setting")
 
     print(story.rss_entry_data.link)
-
+    print(story.rss_entry()["link"])
     b = story.dump()
 
     new_story: BaseStory = BaseStory.load(b)
