54c974bb373ac6ea94b62e1e1c864424024c6b23
Phil Budne <pbudne@angwin.cs.umass.edu>
2023-06-19 14:44:47 +0000

    Initial version of worker.py (passes mypy, without Story object support)
    requirements.txt: add argparse, types-pika
    new: indexer/app.py -- App class (base class for Worker)
    new: indexer/__init__.py -- make indexer/*.py importable (empty)
    new: indexer/scripts/README.md -- readme for runable non-worker scripts
    new: indexer/scripts/log.py -- runnable App to log from shell scripts
    new: bin/pre-commit-wrapper.py -- installs requirements before running mypy
    new: indexer/worker.py -- base class for queue-based workers
    .pre-commit-config.yaml -- run bin/pre-commit-wrapper.py for mypy
    	run isort w/ "black" profile (move to pyproject.toml??)
    .gitignore -- add *~ (emacs backup files)

---
+++ indexer/__init__.py
@@ -0,0 +1,7 @@
+"""
+indexer/__init__.py -- make indexer.thing importable
+
+Different schools of thought exist on what might belong here, so
+discussion/agreement should be reached before adding more than a
+__version__ variable.
+"""
---
+++ indexer/scripts/README.md
@@ -0,0 +1,1 @@
+Programs to be invoked from the command line that are NOT pipeline workers
