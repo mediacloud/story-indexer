ae2075bd4ff8bc9d54cf9c79a03cac6600feae72
Phil Budne <pbudne@angwin.cs.umass.edu>
2023-06-21 18:27:29 +0000

    Add indexer/workers/parser.py (content metadata extractor)
    NOTE!! requires "touch ....venv/python3.X/site-packages/mcmetadata/py.typed"
    for type checking of mcmetadata.extract call

---
+++ indexer/workers/parser.py
@@ -0,0 +1,54 @@
+"""
+metadata parser pipeline worker
+"""
+
+import datetime as dt
+import gzip
+import json
+import logging
+
+# PyPI:
+import mcmetadata
+from pika.adapters.blocking_connection import BlockingChannel
+
+# local:
+from indexer.story import BaseStory
+from indexer.worker import StoryWorker, run
+
+logger = logging.getLogger(__name__)
+
+
+class Parser(StoryWorker):
+    def process_story(
+        self,
+        chan: BlockingChannel,
+        story: BaseStory,
+    ) -> None:
+        rss = story.rss_entry()
+        raw = story.raw_html()
+
+        link = rss.link
+        if link:
+            if raw.html:
+                # XXX WRONG!! should honor charset info in HTTP Content-Type header!
+                html = raw.html.decode("utf-8")
+            else:
+                html = ""  # XXX error?
+
+            # metadata dict
+            # may raise mcmetadata.exceptions.BadContentError
+            mdd = mcmetadata.extract(link, html)
+
+            with story.content_metadata() as cmd:
+                # XXX assumes identical item names!!
+                #       could copy items individually with type checking
+                #       mcmetadata returned TypedDict?
+                for key, val in mdd.items():
+                    setattr(cmd, key, val)
+        # XXX else quarantine?!
+
+        self.send_story(chan, story)
+
+
+if __name__ == "__main__":
+    run(Parser, "parser", "metadata parser worker")
