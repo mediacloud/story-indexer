1756a9df77ac10e3273e0d9b5522df46991cc8d5
Phil Budne <pbudne@angwin.cs.umass.edu>
2023-07-06 19:44:21 +0000

    made Pipeline a QApp, add dummy commands

--- indexer/pipeline.py
+++ indexer/pipeline.py
@@ -7,7 +7,14 @@ an output exchange WORKERNAME-out with links to downstream input
 queues.
 """
 
-from typing import Dict, List, Set, Union
+import argparse
+import sys
+from typing import Callable, Dict, List, Set, Union, cast
+
+# local:
+from indexer.worker import QApp
+
+COMMANDS: List[str] = []
 
 
 class PipelineError(RuntimeError):
@@ -62,11 +69,17 @@ class Consumer(Worker):
         super().__init__(pipeline, name, inputs, False)
 
 
-class Pipeline:
-    def __init__(self, name: str):
-        self.name = name
+def command(func: Callable) -> Callable:
+    """decorator for Pipeline command methods"""
+    COMMANDS.append(func.__name__)
+    return func
+
+
+class Pipeline(QApp):
+    def __init__(self, name: str, descr: str):
         self.queues: Set = set()
         self.workers: Dict[str, Process] = {}
+        super().__init__(name, descr)
 
     def add_producer(self, name: str) -> Producer:
         """Add a Process with no inputs"""
@@ -79,10 +92,46 @@ class Pipeline:
         """Add a Process with no output"""
         return Consumer(self, name, inputs)
 
+    def define_options(self, ap: argparse.ArgumentParser) -> None:
+        super().define_options(ap)
+        ap.add_argument("command", nargs=1, type=str, choices=COMMANDS, help="Command")
+
+    @command
+    def configure(self) -> None:
+        """configure queues"""
+        print("HERE: configure")
+
+    @command
+    def delete(self) -> None:
+        """delete queues"""
+        print("HERE: delete")
+
+    @command
+    def help(self) -> None:
+        """give this output"""
+        # XXX maybe append this to --help output?
+        print("Commands (use --help for options):")
+        for cmd in COMMANDS:
+            descr = self.get_command_func(cmd).__doc__
+            print(f"{cmd:12.12} {descr}")
+
+    def get_command_func(self, cmd: str) -> Callable:
+        """returns command function as a bound method"""
+        return cast(Callable, getattr(self, cmd))
+
+    def main_loop(self) -> None:
+        assert self.args
+        cmds = self.args.command
+        assert cmds
+
+        cmd = cmds[0]
+        self.get_command_func(cmd)()
+
 
 if __name__ == "__main__":
-    p = Pipeline("test")
+    p = Pipeline("test", "test of pipeline configurator")
     p.add_consumer("c", [p.add_worker("b", [p.add_producer("a")])])
 
     print(p.queues)
     print(p.workers)
+    p.main()
