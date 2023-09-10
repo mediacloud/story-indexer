40daf247c4c061dca4b2a1c0c49bea230e623357
Phil Budne <pbudne@angwin.cs.umass.edu>
2023-09-10 02:38:36 +0000

    add node stats

--- indexer/scripts/rabbitmq-stats.py
+++ indexer/scripts/rabbitmq-stats.py
@@ -24,8 +24,22 @@ class QStats(QApp):
     AUTO_CONNECT = False  # never connects (uses AdminAPI)!
 
     def main_loop(self) -> None:
-        seconds = 60  # XXX make a command line option?
+        # XXX make a command line option?
+        # _could_ use 20sec (default graphite recording period)
+        seconds = 60
+
         api = self.admin_api()
+
+        def g(input: Dict[str, Any], item: str, prefix: str) -> None:
+            if input and item in input:
+                value = input[item]
+            else:
+                # report everything, even if missing
+                value = 0
+            path = f"{prefix}.{item}"
+            logger.debug("%s: %s", path, value)
+            self.gauge(path, value)
+
         while True:
             # PLB: FEH!  All the bother to make AdminMixin, and the URL I want isn't included!
             # Looks like the core of AdminAPI doesn't do pagination either!!!
@@ -34,33 +48,30 @@ class QStats(QApp):
             for q in queues:
                 prefix = f"queues.{q['name']}"
 
-                def g(input: Dict[str, Any], item: str) -> None:
-                    if input and item in input:
-                        value = input[item]
-                    else:
-                        # report everything, even if missing
-                        value = 0
-                    path = f"{prefix}.{item}"
-                    logger.debug("%s: %s", path, value)
-                    self.gauge(path, value)
-
-                g(q, "memory")
-                g(q, "messages_ready")
-                g(q, "messages_unacknowledged")
+                g(q, "memory", prefix)
+                g(q, "messages_ready", prefix)
+                g(q, "messages_unacknowledged", prefix)
 
                 ms = q.get("message_stats", None)
-                g(ms, "ack")
-                g(ms, "deliver")
-                g(ms, "publish")
-                g(ms, "redeliver")
-                g(ms, "consumers")
+                g(ms, "ack", prefix)
+                g(ms, "deliver", prefix)
+                g(ms, "publish", prefix)
+                g(ms, "redeliver", prefix)
+                g(ms, "consumers", prefix)
 
-            # MORE???
-            # https://www.datadoghq.com/blog/rabbitmq-monitoring/
-            # exchange: unroutable??
-            # node: fds, sockets, disk used, memory used
+            nodes = api.list_nodes()
+            if nodes:
+                n0 = nodes[0]
+            else:
+                n0 = {}
+            prefix = "node"
+            g(n0, "fd_used", prefix)
+            g(n0, "mem_used", prefix)
+            g(n0, "sockets_used", prefix)
 
-            # nodes = api.list_nodes()
+            # exchange: unroutable?? (need to skip SEMAPHORE!)
+            # exchanges = api.list_exchanges()
+            # List w/ {"name": "foo", "message_stats": { ... }}
 
             # sleep until top of next period:
             sleep_sec = seconds - time.time() % seconds
