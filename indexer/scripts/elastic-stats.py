2939b86030c7468ccb08581a073eaf0cd48b3a50
Phil Budne <pbudne@angwin.cs.umass.edu>
2023-10-10 23:49:23 -0400

    report stats!

--- indexer/scripts/elastic-stats.py
+++ indexer/scripts/elastic-stats.py
@@ -7,8 +7,9 @@ report Elastic Search stats to statsd
 
 import argparse
 import time
+from collections import Counter
 from logging import getLogger
-from typing import Any, Dict
+from typing import Any, Dict, List, cast
 
 from elastic_transport import ConnectionError, ConnectionTimeout
 
@@ -18,7 +19,7 @@ from indexer.elastic import ElasticMixin
 logger = getLogger("elastic-stats")
 
 
-class ElasticStats(App, ElasticMixin):
+class ElasticStats(ElasticMixin, App):
     def define_options(self, ap: argparse.ArgumentParser) -> None:
         super().define_options(ap)
 
@@ -34,7 +35,32 @@ class ElasticStats(App, ElasticMixin):
 
         while True:
             try:
-                print(es.cat.indices(bytes="mb", pri=True))
+                # limit to columns of interest?
+                indices = cast(
+                    List[Dict[str, str]],
+                    es.cat.indices(bytes="mb", pri=True, format="json"),
+                )
+                health: Counter[str] = Counter()
+                for index in indices:
+                    name = index["index"]
+                    logger.debug("index: %s", name)
+
+                    def by_index(input: str, out: str) -> None:
+                        val = int(index[input])
+                        logger.debug(" %s %s %d", input, out, val)
+                        self.gauge("indices." + out, val, labels=[("index", name)])
+
+                    by_index("docs.count", "docs")
+                    by_index("docs.deleted", "deleted")
+                    by_index("pri.store.size", "pri-size")
+                    health[index["health"]] += 1
+
+                # report totals for each health state
+                for color in ("green", "red", "yellow"):
+                    count = health[color]
+                    logger.debug("%s %d", color, count)
+                    self.gauge("indices.health", count, labels=[("color", color)])
+
             except (ConnectionError, ConnectionTimeout) as e:
                 logger.debug("indices: %r", e)
 
