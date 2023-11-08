17218ee40f510183c56c8547b512e22efa628aea
Xavier Frankline <xf.xavierfrank@gmail.com>
2023-11-08 15:12:06 +0300

    deploy changes

--- docker/elastic-deploy.sh
+++ docker/elastic-deploy.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-# Deploy story-indexer
 
 #Script Parameters
 CLUSTER_NAME="mc_elasticsearch"
-ES_VERSION="8.9.0"
-DISCOVERY_ENDPOINTS="woodward.angwin:9300,ramos.angwin:9300,bradley.angwin:9300"
+ES_VERSION="8.x"
+DISCOVERY_ENDPOINTS="localhost"
+# DISCOVERY_ENDPOINTS="woodward.angwin:9300,ramos.angwin:9300,bradley.angwin:9300"
 NETWORK_HOST=""
-DATA_BASE="/srv/data"
+DATA_BASE="/home/ubuntu/data"
 
 help()
 {
-    #TODO: Add help text here
     echo "This script installs Elasticsearch cluster on Ubuntu"
     echo "Parameters:"
     echo "-n elasticsearch cluster name"
-    echo "-d static discovery endpoints 10.0.0.1-3"
+    echo "-d static discovery endpoints"
     echo "-v elasticsearch version"
     echo "-h view this help content"
 }
@@ -28,7 +27,7 @@ if [ "${UID}" -ne 0 ];
 then
     log "Script executed without root permissions"
     echo "You must be root to run this program." >&2
-    exit 3
+    exit 1
 fi
 
 #Loop through options passed
@@ -107,65 +106,96 @@ set_network_host() {
   fi
 }
 
+
+start_elasticsearch_service() {
+    log "Starting Elasticsearch service on $HOSTNAME"
+    sudo systemctl start elasticsearch
+    if systemctl is-active --quiet elasticsearch; then
+        log "Elasticsearch service started successfully"
+    else
+        log "Failed to start Elasticsearch service"
+        exit 1
+    fi
+    sudo systemctl daemon-reload
+    sudo systemctl enable elasticsearch
+}
+
 # Primary Install Tasks
 install_es
 
-if [ -d "$DATA_BASE" ]; then
-    setup_data_disk "$DATA_BASE"
+if [ $? -eq 0 ]; then
+    if [ -d "$DATA_BASE" ]; then
+        setup_data_disk "$DATA_BASE"
+    else
+        log "Error: $DATA_BASE does not exist or is not a directory."
+    fi
+
+    if [[ -n "$DISCOVERY_ENDPOINTS" ]]; then
+      HOSTS_CONFIG=("[\"${DISCOVERY_ENDPOINTS//,/\",\"}\"]")
+      echo "discovery.seed_hosts: ${HOSTS_CONFIG}"
+    else
+      echo "DISCOVERY_ENDPOINTS is empty. Please provide discovery endpoints."
+    fi
+
+    DATAPATH=$DATA_BASE/elasticsearch/data
+    LOGPATH=$DATA_BASE/elasticsearch/logs
+
+    # Configure Elasticsearch settings
+    #---------------------------
+    #Backup the current Elasticsearch configuration file
+    mv /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.bak
+
+    # Set cluster and machine names - just use hostname for our node.name
+    echo "cluster.name: $CLUSTER_NAME" >> /etc/elasticsearch/elasticsearch.yml
+    echo "node.name: $HOSTNAME" >> /etc/elasticsearch/elasticsearch.yml
+    echo "path.data: $DATAPATH" >> /etc/elasticsearch/elasticsearch.yml
+    echo "path.logs: $LOGPATH" >> /etc/elasticsearch/elasticsearch.yml
+    echo "network.host: $NETWORK_HOST" >> /etc/elasticsearch/elasticsearch.yml
+    echo "discovery.seed_hosts: $HOSTS_CONFIG" >> /etc/elasticsearch/elasticsearch.yml
+
+    # Disable xpack.security features
+    # We're mostly accessing our ES instance withing a local network
+    echo "xpack.security.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
+    echo "xpack.security.enrollment.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
+    echo "xpack.security.http.ssl.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
+    echo "xpack.security.transport.ssl.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
+    # PS:Default transport and http ports are 9300 and 9200
+    #echo "http.port: 9200" >> /etc/elasticsearch/elasticsearch.yml
+    #transport.port: 9300 >> /etc/elasticsearch/elasticsearch.yml
+
+    #Lock memory on startup
+    echo "bootstrap.memory_lock: true" >> /etc/elasticsearch/elasticsearch.yml
+
+    ELASTIC_SERVICE_FILE="/usr/lib/systemd/system/elasticsearch.service"
+    if [ ! -f "$ELASTIC_SERVICE_FILE" ]; then
+        echo "Service file not found: $ELASTIC_SERVICE_FILE"
+        exit 1
+    fi
+
+    sed -i '/^\[Service\]/a LimitMEMLOCK=infinity' "$ELASTIC_SERVICE_FILE"
+
+    if [ $? -eq 0 ]; then
+        echo "LimitMEMLOCK set to infinity in $ELASTIC_SERVICE_FILE"
+        return 0
+    else
+        echo "Failed to set LimitMEMLOCK in $ELASTIC_SERVICE_FILE"
+        return 1
+    fi
+    systemctl daemon-reload
+
+    # Update HEAP Size in this configuration or in upstart service
+    CUSTOM_JVM_OPTIONS_FILE="/etc/elasticsearch/jvm.options.d/mc_jvm.options"
+
+    mkdir -p "$(dirname "$CUSTOM_JVM_OPTIONS_FILE")"
+    # Set the minimum and maximum heap size settings (Xms and Xmx) in the custom jvm.options file
+    echo "-Xms30g" > "$CUSTOM_JVM_OPTIONS_FILE"
+    echo "-Xmx30g" >> "$CUSTOM_JVM_OPTIONS_FILE"
+    echo "Custom JVM options have been set in $CUSTOM_JVM_OPTIONS_FILE"
+
 else
-    log "Error: $DATA_BASE does not exist or is not a directory."
+  echo "Elasticsearch installation failed. Aborting further setup."
+  exit 1
+
 fi
 
-if [[ -n "$DISCOVERY_ENDPOINTS" ]]; then
-  HOSTS_CONFIG=("[\"${DISCOVERY_ENDPOINTS//,/\",\"}\"]")
-  echo "discovery.seed_hosts: ${HOSTS_CONFIG}"
-else
-  echo "DISCOVERY_ENDPOINTS is empty. Please provide discovery endpoints."
-fi
-
-DATAPATH=$DATA_BASE/elasticsearch/data
-LOGPATH=$DATA_BASE/elasticsearch/logs
-
-# Configure Elasticsearch settings
-#---------------------------
-#Backup the current Elasticsearch configuration file
-mv /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.bak
-
-# Set cluster and machine names - just use hostname for our node.name
-echo "cluster.name: $CLUSTER_NAME" >> /etc/elasticsearch/elasticsearch.yml
-echo "node.name: $HOSTNAME" >> /etc/elasticsearch/elasticsearch.yml
-echo "path.data: $DATAPATH" >> /etc/elasticsearch/elasticsearch.yml
-echo "path.logs: $LOGPATH" >> /etc/elasticsearch/elasticsearch.yml
-echo "network.host: $NETWORK_HOST" >> /etc/elasticsearch/elasticsearch.yml
-echo "discovery.seed_hosts: $HOSTS_CONFIG" >> /etc/elasticsearch/elasticsearch.yml
-
-# Disable xpack.security features
-# We're mostly accessing our ES instance withing a local network
-echo "xpack.security.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
-echo "xpack.security.enrollment.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
-echo "xpack.security.http.ssl.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
-echo "xpack.security.transport.ssl.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
-# PS:Default transport and http ports are 9300 and 9200
-#echo "http.port: 9200" >> /etc/elasticsearch/elasticsearch.yml
-#transport.port: 9300 >> /etc/elasticsearch/elasticsearch.yml
-
-#Lock memory on startup
-echo "bootstrap.memory_lock: true" >> /etc/elasticsearch/elasticsearch.yml
-
-# Update HEAP Size in this configuration or in upstart servic
-CUSTOM_JVM_OPTIONS_FILE="/etc/elasticsearch/jvm.options.d/mc_jvm.options"
-
-mkdir -p "$(dirname "$CUSTOM_JVM_OPTIONS_FILE")"
-
-# Set the minimum and maximum heap size settings (Xms and Xmx) in the custom jvm.options file
-cat <<EOL > "$CUSTOM_JVM_OPTIONS_FILE"
--Xms30g
--Xmx30g
-EOL
-echo "Custom JVM options have been set in $CUSTOM_JVM_OPTIONS_FILE"
-
-
-log "Starting Elasticsearch on ${HOSTNAME}"
-sudo systemctl start elasticsearch
-sudo systemctl enable elasticsearch
-log "complete elasticsearch setup and started"
+start_elasticsearch_service
