50f6e9297bf1b1c9b897d6f9099c2306d2bd4263
Xavier Frankline <xf.xavierfrank@gmail.com>
2023-12-05 08:55:55 +0300

    default sentry env

--- indexer/sentry.py
+++ indexer/sentry.py
@@ -13,12 +13,10 @@ def init() -> bool:
     """
 
     sentry_dsn = os.environ.get("SENTRY_DSN")
-    sentry_environment = os.environ.get("SENTRY_ENVIRONMENT")
 
     if sentry_dsn:
         sentry_sdk.init(
             dsn=sentry_dsn,
-            environment=sentry_environment,
             # By default the SDK will try to use the SENTRY_RELEASE
             # environment variable, or infer a git commit
             # SHA as release, however you may want to set
