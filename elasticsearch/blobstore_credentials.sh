d144b1da712d36a2f4a3d9867fb3801d6f1cc0ac
Xavier Frankline <xf.xavierfrank@gmail.com>
2024-07-05 13:55:29 +0300

    updates: login and doc

--- elasticsearch/blobstore_credentials.sh
+++ elasticsearch/blobstore_credentials.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
-# Configure Elasticsearch S3 credentials to elasticsearch-keystore
-# set s3.access_key and s3.secret_key, also for Backblaze
+# This script configures Elasticsearch to use cloud storage credentials for snapshot and restore operations.
+# It supports both Amazon S3 and Backblaze B2 as the cloud storage options.
+#
+# Usage: ./script.sh -b <blobstore>
+#
+# Options:
+#   -b <blobstore>      Specify the cloud storage type ('s3' for Amazon S3, 'b2' for Backblaze B2).
+#   -h                  Display this help message and exit.
+#
+# Notes:
+# - This script must be run as the root user.
+# - Ensure Elasticsearch is running and accessible at http://localhost:9200 before executing this script.
+
+# Example:
+#   ./elasticsearch/blobstore_credentials.sh -b s3
+#
+
 ES_PATH="/usr/share/elasticsearch"
 KEY_NAME_PREFIX="s3.client.default"
 ENV_FILE="prod"
@@ -22,10 +37,13 @@ help() {
     echo ""
     echo "Options:"
     echo "  -b <blobstore>      Specify the blobstore type (s3 or b2)"
-    echo "  -c <container_name> Specify the Elasticsearch container name (if running in Docker)"
     echo "  -h                  Show this help message"
     echo ""
 }
+
+log() {
+        echo "$1"
+    }
 
 zzz() {
     echo $1 | tr 'A-Za-z' 'N-ZA-Mn-za-m'
@@ -48,32 +66,33 @@ while getopts "b:h" opt; do
 done
 
 if [ $(whoami) != "root" ]; then
-    echo "This script must be run as root."
+    log "ERROR: This script must be run as root."
     exit 1
 fi
 
 if [ -z "$BLOBSTORE" ]; then
-    show_help
+    help
     exit 1
 fi
 
 if [ "$BLOBSTORE" != "s3" ] && [ "$BLOBSTORE" != "b2" ]; then
-    echo "Invalid blobstore type. Use 's3' or 'b2'."
+    log "ERROR: Invalid blobstore type. Use 's3' or 'b2'."
     exit 1
 fi
 
 PRIVATE_CONF_DIR="es-credentials-setup"
 run_as_login_user mkdir -p $PRIVATE_CONF_DIR
 chmod go-rwx $PRIVATE_CONF_DIR
+log "INFO: Created private configuration directory $PRIVATE_CONF_DIR"
 
 cd $PRIVATE_CONF_DIR
 CONFIG_REPO_PREFIX=$(zzz tvg@tvguho.pbz:zrqvnpybhq)
 CONFIG_REPO_NAME=$(zzz fgbel-vaqrkre-pbasvt)
 PRIVATE_CONF_REPO=$(pwd)/$CONFIG_REPO_NAME
 
-echo "Cloning $CONFIG_REPO_NAME repo" 1>&2
+log "INFO: Cloning $CONFIG_REPO_NAME repo" 1>&2
 if ! run_as_login_user git clone "$CONFIG_REPO_PREFIX/$CONFIG_REPO_NAME.git" >/dev/null 2>&1; then
-    echo "FATAL: could not clone config repo" 1>&2
+    log "FATAL: could not clone config repo" 1>&2
     exit 1
 fi
 
@@ -81,7 +100,7 @@ PRIVATE_CONF_FILE="$PRIVATE_CONF_REPO/$ENV_FILE.sh"
 cd ..
 
 if [ ! -f "$PRIVATE_CONF_FILE" ]; then
-    echo "FATAL: could not access $PRIVATE_CONF_FILE" 1>&2
+    log "FATAL: could not access $PRIVATE_CONF_FILE" 1>&2
     exit 1
 fi
 
@@ -106,30 +125,32 @@ check_elasticsearch() {
 }
 
 add_credentials() {
+    log "INFO: Adding credentials to the Elasticsearch keystore"
     echo "$ACCESS_KEY" | $ES_PATH/bin/elasticsearch-keystore add --stdin --force "$KEY_NAME_PREFIX.access_key"
     echo "$SECRET_KEY" | $ES_PATH/bin/elasticsearch-keystore add --stdin --force "$KEY_NAME_PREFIX.secret_key"
 }
 
 # Function to reload Elasticsearch secure settings
 reload_secure_settings() {
+    log "INFO: Reloading Elasticsearch secure settings"
     curl -X POST "http://localhost:9200/_nodes/reload_secure_settings" -H "Content-Type: application/json" -d '{}'
 }
 
 if check_elasticsearch; then
-    add_credentialsgi
+    add_credentials
     if [ $? -eq 0 ]; then
         reload_secure_settings
         if [ $? -eq 0 ]; then
-            echo "Credentials for $BLOBSTORE added and secure settings reloaded successfully."
+            log "INFO: Credentials for $BLOBSTORE added and secure settings reloaded successfully."
         else
-            echo "Failed to reload secure settings."
+            log "ERROR: Failed to reload secure settings."
             exit 1
         fi
     else
-        echo "Failed to add credentials to the keystore."
+        log "ERROR: Failed to add credentials to the keystore."
         exit 1
     fi
 else
-    echo "Elasticsearch is not running or reachable. Exiting."
+    log "ERROR: Elasticsearch is not running or reachable. Exiting."
     exit 1
 fi
