f43bc9cfdc09a406e8e7c794b85dd009c23c0514
Xavier Frankline <xf.xavierfrank@gmail.com>
2024-10-29 16:51:12 +0300

    chore: update re-index.sh script and documentation

--- bin/run-elastic-reindex.sh
+++ bin/run-elastic-reindex.sh
@@ -11,23 +11,71 @@
 #
 
 # Usage:
-# Modify the variables ES_HOST, SOURCE_INDEX, and DEST_INDEX as needed.
+# Modify the variables ES_HOST, SOURCE_INDEX, and DEST_SUFFIX and MAX_DOCS as needed.
 # Run this script using: sh reindex.sh
-# ./run-elastic-reindex.sh ES_HOST SOURCE_INDEX DEST_INDEX
+# ./run-elastic-reindex.sh @
 #
 
-# Example:
-# ./run-elastic-reindex.sh mc-search-00001 mc-search-v2-00001
+display_help() {
+    echo "Elasticsearch Reindexing Script"
+    echo
+    echo "Usage: sh run-elastic-reindex.sh [-h] [-e ES_HOST] -s SOURCE_INDEX... -d DEST_SUFFIX"
+    echo "Example: sh run-elastic-reindex.sh -e http://localhost:9200 -s mc_search-000001 mc_search-000002 -d reindexed -m 1000"
+    echo
+    echo "Arguments:"
+    echo "  -h            Show help information."
+    echo "  -e ES_HOST    Optional. The URL of the Elasticsearch host (default: http://localhost:9200)."
+    echo "  -s SOURCE_INDEX Required. One or more source indices to reindex from (space-separated)."
+    echo "  -d DEST_SUFFIX Required. Suffix for the destination index names."
+    echo "  -m MAX_DOCS   Optional. The maximum number of documents to re-index. Must be a positive integer."
+    echo
+}
 
-# Default values
-ES_HOST=${1:-"http://localhost:9200"}
-SOURCE_INDEX=$2
-DEST_INDEX=$3
+ES_HOST="http://localhost:9210"
 OP_TYPE="create"  # Operation type for reindex, could be `create` or `index`
+SOURCE_INDEXES=() # Array to hold source indices
+DEST_SUFFIX="" #Suffix destination indexes
+MAX_DOCS="" # Maximum number of documents to reindex
 
-if [ -z "$SOURCE_INDEX" ] || [ -z "$DEST_INDEX" ]; then
-    echo "Error: Both SOURCE_INDEX and DEST_INDEX are required."
-    echo "Usage: sh run-elastic-reindex.sh [ES_HOST] SOURCE_INDEX DEST_INDEX"
+while getopts ":he:s:d:m:" opt; do
+    case $opt in
+        h)
+            display_help
+            exit 0
+            ;;
+        e)
+            ES_HOST=$OPTARG
+            ;;
+        s)
+            shift $((OPTIND - 2))
+            while [[ "$1" != -* && -n "$1" ]]; do
+                SOURCE_INDEXES+=("$1")
+                shift
+            done
+            OPTIND=1
+            ;;
+        d)
+            DEST_SUFFIX=$OPTARG
+            ;;
+        m)
+            MAX_DOCS=$OPTARG
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            display_help
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            display_help
+            exit 1
+            ;;
+    esac
+done
+
+if [ ${#SOURCE_INDEXES[@]} -eq 0 ] || [ -z "$DEST_SUFFIX" ]; then
+    echo "Error: At least one SOURCE_INDEX and a DEST_SUFFIX are required."
+    display_help
     exit 5
 fi
 
@@ -41,43 +89,60 @@ check_es_alive() {
 }
 
 check_source_index_exists() {
-    response=$(curl -s -o /dev/null -w "%{http_code}" "$ES_HOST/$SOURCE_INDEX")
-    if [ "$response" -ne 200 ]; then
-        echo "Error: Source index '$SOURCE_INDEX' does not exist."
-        exit 2
-    fi
-    echo "Source index '$SOURCE_INDEX' exists."
+    for index in "${SOURCE_INDEXES[@]}"; do
+        response=$(curl -s -o /dev/null -w "%{http_code}" "$ES_HOST/$index")
+        if [ "$response" -ne 200 ]; then
+            echo "Error: Source index '$index' does not exist."
+            exit 2
+        fi
+        echo "Source index '$index' exists."
+    done
 }
 
 check_dest_index_not_exists() {
-    response=$(curl -s -o /dev/null -w "%{http_code}" "$ES_HOST/$DEST_INDEX")
-    if [ "$response" -eq 200 ]; then
-        echo "Error: Destination index '$DEST_INDEX' already exists."
-        exit 3
-    fi
-    echo "Destination index '$DEST_INDEX' does not exist."
+    for index in "${SOURCE_INDEXES[@]}"; do
+        DEST_INDEX="${index}-${DEST_SUFFIX}"
+        response=$(curl -s -o /dev/null -w "%{http_code}" "$ES_HOST/$DEST_INDEX")
+        if [ "$response" -eq 200 ]; then
+            echo "Error: Destination index '$DEST_INDEX' already exists."
+            exit 3
+        fi
+        echo "Destination index '$DEST_INDEX' does not exist."
+    done
 }
+
+# From ES https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-reindex.html#docs-reindex-from-multiple-sources
+# Indexing multiple sources
 
 start_reindex() {
-    echo "Starting reindex from '$SOURCE_INDEX' to '$DEST_INDEX'..."
-    task_response=$(curl -s -X POST "$ES_HOST/_reindex?wait_for_completion=false" \
-    -H 'Content-Type: application/json' -d "
-    {
-      \"source\": { \"index\": \"$SOURCE_INDEX\" },
-      \"dest\": { \"index\": \"$DEST_INDEX\", \"op_type\": \"$OP_TYPE\" }
-    }")
+    for index in "${SOURCE_INDEXES[@]}"; do
+        DEST_INDEX="${index}-${DEST_SUFFIX}"
+        echo "Starting reindex from '$index' to '$DEST_INDEX'..."
 
-    task_id=$(echo "$task_response" | grep -o '"task":"[^"]*"' | cut -d':' -f2 | tr -d '"')
+        reindex_body="{\"source\": {\"index\": \"$index\"}, \"dest\": {\"index\": \"$DEST_INDEX\", \"op_type\": \"$OP_TYPE\"}"
 
-    if [ -z "$task_id" ]; then
-        echo "Error: Failed to start reindexing task."
-        exit 4
-    fi
+        if [ -n "$MAX_DOCS" ]; then
+            reindex_body="${reindex_body}, \"max_docs\": $MAX_DOCS"
+        fi
 
-    echo "Reindexing task started with task ID: $task_id"
+        reindex_body="${reindex_body}}"
+
+        echo "Reindex body: $reindex_body"
+
+        task_response=$(curl -s -X POST "$ES_HOST/_reindex?wait_for_completion=false" \
+        -H 'Content-Type: application/json' -d "$reindex_body")
+
+        task_id=$(echo "$task_response" | grep -o '"task":"[^"]*"' | cut -d':' -f2- | tr -d '"')
+
+        if [ -z "$task_id" ]; then
+            echo "Error: Failed to start reindexing task for index '$index'."
+            exit 4
+        fi
+
+        echo "Reindexing task started for index '$index' with task ID: $task_id"
+    done
 }
 
-# Main script execution
 check_es_alive
 check_source_index_exists
 check_dest_index_not_exists
--- doc/elasticsearch/elasticsearch_reindex.md
+++ doc/elasticsearch/elasticsearch_reindex.md
@@ -4,7 +4,7 @@ The _reindex API in Elasticsearch allows you to copy documents from one index to
 This huide covers reindexing using two methods;
 
 1. Using the Kibana Dev Tools
-2. Using `curl` and Elasticsearch `reindex` API as per the script [here](bin/run-elastic-reindex.sh)
+2. Using `curl` and Elasticsearch `reindex` API as per the script [here](../../bin/run-elastic-reindex.sh)
 
 ### Reindexing with Kibana Dev Tools
 
@@ -30,5 +30,47 @@ POST _reindex
 
 ```
 
-**Replace source-index-name and dest-index-name with the names of your source and destination indices.
-***Set the "op-type":"create" to avoid overwriting existing documents in the destination index. To allow overwriting use "op_type":"index"
+*** Replace source-index-name and dest-index-name with the names of your source and destination indices.
+
+*** Set the "op-type":"create" to avoid overwriting existing documents in the destination index. To allow overwriting use "op_type":"index"
+
+3. Use the following GET request from the Kibana Dev Tools console to get the Reindexing status
+
+```
+GET _tasks/<task_id>
+
+```
+
+### Reindexing with Curl & Elasticsearch Reindex API
+
+The Elasticsearch Reindex API provides for a REST endpoint to re-index documents.
+The bash script available [here](../../bin/run-elastic-reindex.sh), allows for re-indexing by performing the necessary checks, and initiating the re-indexing process asynchronously.
+
+The script returns a `task ID` that can be used to monitor the Reindexing status via curl command
+
+```
+curl -X GET "http://localhost:9200/_tasks/<task_id>"
+```
+
+#### Reindexing Limited Number of Documents for Testing
+
+The Elasticsearch Reindex API provides for a `max_docs` argument to specify the maximum number of documents to reindex.
+
+```
+{
+    "source": {
+        "index": "mc_search-000002"
+    },
+    "dest": {
+        "index": "mc_search-000002-test",
+        "op_type": "create"
+    },
+    "max_docs": 10
+}
+```
+
+The [script](../../bin/run-elastic-reindex.sh) provides for an optional argument `-m` to specify the number of documents to re-index.
+
+#### Reindexing from Multiple sources
+
+Elasticsearch recommends to index one document at a time if we have many indices to reindex from, as referenced [here](https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-reindex.html#docs-reindex-from-multiple-sources).
