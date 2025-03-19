172b65cdf8da5c6d1ce31b0b8053ddaac069cce8
Xavier Frankline <xf.xavierfrank@gmail.com>
2025-03-19 11:28:17 +0300

    Fix variable order precedence on playbooks refactor es-test-vars.sh

--- conf/ansible/es-test-vars.sh
+++ conf/ansible/es-test-vars.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-
 # command to run es-test-vars.yml playbook
 # for testing es-inventory.yml and es-vars.yml
+# Usage ./es-test-vars.sh <environment>
+# e.g ./es-test-vars.sh production
+ENVIRONMENT=${1:-staging}
 
 make setup_venv
+
 venv/bin/ansible-playbook \
-    -i inventories/staging/hosts.yml \
-    "$@" \
+    -i "inventories/${ENVIRONMENT}/hosts.yml" \
+    -e "env=${ENVIRONMENT}" \
+    "${@:2}" \
     es-test-vars.yml
