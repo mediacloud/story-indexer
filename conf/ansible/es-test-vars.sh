f18d91a469241e9d7aec0cc6a958d315c3d94640
Xavier Frankline <xf.xavierfrank@gmail.com>
2025-03-19 15:15:14 +0300

    Update --add tasks to create index_template, initial_index        -- add templates, index_template, ilm_policy, slm_policy

--- conf/ansible/es-test-vars.sh
+++ conf/ansible/es-test-vars.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 # command to run es-test-vars.yml playbook
 # for testing es-inventory.yml and es-vars.yml
-# Usage ./es-test-vars.sh <environment>
-# e.g ./es-test-vars.sh production
+# Usage: ./es-test-vars.sh <environment> [ansible-playbook options]
+# Example: ./es-test-vars.sh production
+
+# Set default environment to "staging" if not provided
 ENVIRONMENT=${1:-staging}
-
 make setup_venv
-
+shift
 venv/bin/ansible-playbook \
     -i "inventories/${ENVIRONMENT}/hosts.yml" \
     -e "env=${ENVIRONMENT}" \
-    "${@:2}" \
+    "$@" \
     es-test-vars.yml
