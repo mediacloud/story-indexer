fa72e3b2cbc202de083ba023c38240712b0306d3
Phil Budne <phil@regressive.org>
2026-07-12 01:09:05 -0400

    update to mc-deploy-0.5.1
    add port biases for flavors
    add tag_prod() method!

--- docker/deploy.py
+++ docker/deploy.py
@@ -9,7 +9,22 @@ from mc_deploy.docker import DockerDeploy
 
 class StoryIndexerDeploy(DockerDeploy):
     INST_BASE = "indexer"  # app base name
+
+    # map command line names to inst_base prefix & port bias
+    # ES usesports 9200 & 9300, so bias values increase by 200
+    INST_FLAVORS = {
+        "queue-fetcher": ("", 0),
+        "batch-fetcher": ("", 0),
+        "historical": ("hist-", 200),
+        "archive": ("arch-", 400),
+        "csv": ("csv-", 600),
+    }
     PROJECT_REPO = "story-indexer"
+
+    def tag_prod(self) -> str:
+        # pyproject version not updated.
+        prefix = self.inst_flavor_prefix
+        return f"{self.date_time}-{prefix}prod"
 
 
 d = StoryIndexerDeploy()
